224903d90f48cee7afcb2c87191fe181524f065a
strongdiamond <38755830+strongdiamond@users.noreply.github.com>
2021-02-17 16:44:12 +0800

    Update tif2bit_2_8bit.py
    this code can  convert  tif(2bit)  to tif(8bit)

--- tif2bit_2_8bit.py
+++ tif2bit_2_8bit.py
@@ -1,1 +1,18 @@
 
+# coding: utf-8
+# Import system modules
+import os
+import sys
+import arcpy
+from arcpy import env
+
+
+def tiff2bit_to_8bit(in_tiff2bit,out_tiff8bit):
+    # Check out the ArcGIS Spatial Analyst extension license
+    arcpy.CopyRaster_management(in_tiff2bit,out_tiff8bit,"DEFAULTS","0","9","","","8_BIT_UNSIGNED")
+
+if __name__ == '__main__':
+    # env.workspace=r'D:\sd\clip'
+    in_tiff2bit=r'D:\clip\mask.tif'
+    out_tiff8bit =r'D:\clip\mask8bit.tif'
+    tiff2bit_to_8bit(in_tiff2bit,out_tiff8bit)
